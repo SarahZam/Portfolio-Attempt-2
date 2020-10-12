3d26ec7edbb9f51d062ae08e0345d0b403b047c4
Sarah Zaman <szaman@u.rochester.edu>
2020-10-12 01:55:24 -0400

    Updates

---
+++ precache-manifest.9ce6baf0cffbc196e2b6e745ea155a6e.js
@@ -0,0 +1,118 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b459b3206e243f9a0881a4749a3f9e4a",
+    "url": "/my-portfolio/index.html"
+  },
+  {
+    "revision": "0c8c6f9c0ef73bc4c1ee",
+    "url": "/my-portfolio/static/css/2.e8da32f3.chunk.css"
+  },
+  {
+    "revision": "8fc24d0c82b5392edea1",
+    "url": "/my-portfolio/static/css/main.f97f10d4.chunk.css"
+  },
+  {
+    "revision": "0c8c6f9c0ef73bc4c1ee",
+    "url": "/my-portfolio/static/js/2.7b0c7d9c.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/my-portfolio/static/js/2.7b0c7d9c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8fc24d0c82b5392edea1",
+    "url": "/my-portfolio/static/js/main.aed0db5a.chunk.js"
+  },
+  {
+    "revision": "5d4511f3fcec99c33304",
+    "url": "/my-portfolio/static/js/runtime-main.c58b4907.js"
+  },
+  {
+    "revision": "b2821b20fa057bf735005c609f076837",
+    "url": "/my-portfolio/static/media/cat.b2821b20.jpg"
+  },
+  {
+    "revision": "7534a53c0d3b250d6ca8beb380327d21",
+    "url": "/my-portfolio/static/media/checker.7534a53c.jpg"
+  },
+  {
+    "revision": "eb267f35256591eb7733b73c0455dbfe",
+    "url": "/my-portfolio/static/media/confusion.eb267f35.png"
+  },
+  {
+    "revision": "8e04892dca8b81a9b5a2dc3f36036110",
+    "url": "/my-portfolio/static/media/cs.8e04892d.jpg"
+  },
+  {
+    "revision": "a06440f407802f1fceaef155379bae09",
+    "url": "/my-portfolio/static/media/csug.a06440f4.png"
+  },
+  {
+    "revision": "42ed6957e07727db57cea71da1409e96",
+    "url": "/my-portfolio/static/media/exphci.42ed6957.jpg"
+  },
+  {
+    "revision": "c232e12682294be16fe4ae5729e1cd84",
+    "url": "/my-portfolio/static/media/exppic.c232e126.png"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/my-portfolio/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/my-portfolio/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/my-portfolio/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/my-portfolio/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/my-portfolio/static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "cbf1ba601e7a8fc2e429237ef115393b",
+    "url": "/my-portfolio/static/media/khichuri.cbf1ba60.jpg"
+  },
+  {
+    "revision": "b1ba1c02dc912269fabf00cda16c0449",
+    "url": "/my-portfolio/static/media/pipboy.b1ba1c02.png"
+  },
+  {
+    "revision": "6c66647e1526ac7ba9fb2f98155a27f7",
+    "url": "/my-portfolio/static/media/pong.6c66647e.png"
+  },
+  {
+    "revision": "cfbf00ad39a94bd0f447f6deb28b9e6a",
+    "url": "/my-portfolio/static/media/prank.cfbf00ad.png"
+  },
+  {
+    "revision": "51bae18b87e110d13a47fd10d2a03ed0",
+    "url": "/my-portfolio/static/media/propic.51bae18b.jpg"
+  },
+  {
+    "revision": "c334d01c45601dbb50a43891a105b195",
+    "url": "/my-portfolio/static/media/rmdb.c334d01c.png"
+  },
+  {
+    "revision": "8ec21e4b9e31dc9b7d24f5a4e0170b55",
+    "url": "/my-portfolio/static/media/rtc.8ec21e4b.png"
+  },
+  {
+    "revision": "5ea89f638cd65c43a30ef6d5290051b7",
+    "url": "/my-portfolio/static/media/snake.5ea89f63.jpg"
+  },
+  {
+    "revision": "8feff31a9b3bc01d8c6d290a2b44c8d5",
+    "url": "/my-portfolio/static/media/tictactoe.8feff31a.jpg"
+  },
+  {
+    "revision": "1fc2367c71052e7c94ec0750fa6b3e6a",
+    "url": "/my-portfolio/static/media/uofr.1fc2367c.jpg"
+  }
+]);
