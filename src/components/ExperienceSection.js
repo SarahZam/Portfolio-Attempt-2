904e2c017f2e95f6dd6d1b6403431aec7e82fcdf
Sarah Zaman <szaman@u.rochester.edu>
2020-10-12 00:53:22 -0400

    I'm done???

---
+++ src/components/ExperienceSection.js
@@ -0,0 +1,40 @@
+import React from "react";
+import {Container, Row, Col} from "react-bootstrap";
+
+function ExperienceSection(props) {
+
+    const makeDes = (description) => {
+        return description.map(des => {
+            return <div des={des}  >
+                <ul>
+                    <li>{des}</li>
+                </ul>
+            </div>
+        })
+    }
+
+    return(
+        <Container className="pb-5">
+            <Row>
+                <Col md={12}>
+                    <h3 className="typistcolor border-bottom">{props.exp.company}</h3>
+                </Col>
+            </Row>
+            <Row>
+                <Col md={8}>
+                   <h4 className="mt-2">{props.exp.jobtitle}</h4>
+                </Col>
+                <Col md={4}>
+                    <h5 className="text-right">{props.exp.date}</h5>
+                </Col>
+            </Row>
+            <Row className="mt-2">
+                <Col md={9}>
+                    {makeDes(props.exp.description)}
+                </Col>
+            </Row>
+        </Container>
+    )
+}
+
+export default ExperienceSection;
--- src/pages/ExperiencePage.js
+++ src/pages/ExperiencePage.js
@@ -1,11 +1,78 @@
 import React from 'react';
 import {Container,Row, Col} from "react-bootstrap";
 import Hero from "../components/Hero";
+import yolo from '../assets/images/exppic.png';
+import csug from '../assets/images/csug.png';
+import hci from '../assets/images/exphci.jpg';
+import cs from '../assets/images/cs.jpg';
+import ProjectCards from "../components/ProjectCards";
+import ExperienceSection from "../components/ExperienceSection";
+
+const exps = [
+    {
+        id: 0,
+        company: 'Yolo Shadow',
+        img:yolo,
+        jobtitle: 'Software Engineering Intern',
+        date: 'September 2020 - Current',
+        description: [
+                'I collaborated in a team of 3 to design front-end of the company website using ReactJS, Redux, and Bootstrap.',
+                'I designed and programmed a dashboard option for specific users.',
+                'I developed web structures and cascading sheets in MERN stack.'
+
+        ]
+    },
+    {
+        id: 1,
+        company: 'University of Rochester, Computer Science Undergraduate Council',
+        img: csug,
+        jobtitle: 'Web Developer',
+        date: 'October 2020 - Current',
+        description:[
+            'Leading a team to design an interactive website for our Computer Science Undergraduate Council.',
+            'Coding in HTML, CSS, JavaScript, and ReactJS and ensuring mobile-first strategy. '
+            ]
+    },
+    {
+        id: 2,
+        company: 'University of Rochester, Computer Science Department',
+        img: cs,
+        jobtitle: 'Workshop Leader',
+        date: 'August 2020 - Current',
+        description:[
+            'Conducted weekly hour-long workshops focused on introductory computer science concepts implemented in Java.',
+            'Dedicated weekly office hours and graded exams.'
+        ]
+    },
+    {
+        id: 3,
+        company: 'University of Rochester, Human-Computer Interaction Lab',
+        img: hci,
+        jobtitle: 'Research Assistant',
+        date: 'August 2020 - Current',
+        description:[
+            'Analyzed statistical data using pandas framework and features extracted from audio-visual files by software such as openSMILE and openEAR in order to distinguish good and bad debate practices.',
+            'Collaborated with a team of 3 to collect and annotate over 400 audio-visual files',
+            'Used Python libraries such as Pandas, Matplotlib, and NumPy to analyze the data gathered by the annotation of the transcripts. Graphed the numerical results using Excel and presented the statistical findings with PowerPoint.',
+            'Co-designed and deployed the temporary website for outreach of participants using HTML, CSS and JavaScript'
+        ]
+    }
+
+]
+
 
 function ExperiencePage(props) {
+
+    const makeExps = (exps) => {
+        return exps.map(exp => {
+            return <ExperienceSection exp={exp} key={exp.id} />
+        })
+    }
+
     return(
-        <div>
+        <div className="pb-5">
             <Hero title={props.title}/>
+            {makeExps(exps)}
         </div>
 
     )
